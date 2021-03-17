2b36da82a0c454f4c1bcb8c908b7d32a87a22a1f
Schuyler Manchester <schuyler.manchester@gmail.com>
2021-03-17 15:03:06 -0700

    Add a comment and remove unnecessary swp file.

--- src/main/java/com/schuyler/springboot/Controller.java
+++ src/main/java/com/schuyler/springboot/Controller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// Adding a comment for funzies.
 @RestController
 public class Controller {
 	@GetMapping("/")
