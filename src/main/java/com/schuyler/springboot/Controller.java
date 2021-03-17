ff7744d56e522b649af981ed4d52bbbab42fc2b8
Schuyler Manchester <schuyler.manchester@gmail.com>
2021-03-17 14:44:32 -0700

    Skeleton of gradle springboot application

---
+++ src/main/java/com/schuyler/springboot/Controller.java
@@ -0,0 +1,18 @@
+package com.schuyler.springboot;
+
+import com.schuyler.springboot.response.Default;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	@GetMapping("/")
+	public Default index() {
+		return new Default("Some cool soccer facts coming soon.");
+	}
+}
+
---
+++ src/main/java/com/schuyler/springboot/response/Default.java
@@ -0,0 +1,13 @@
+package com.schuyler.springboot.response;
+
+public class Default {
+  private final String content;
+
+  public Default(String content) {
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
